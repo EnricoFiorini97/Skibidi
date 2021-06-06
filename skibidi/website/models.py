03a8a1d8a1796a16a6246be5d4ae5a86a0781550
Enrico Fiorini <enricofiorini899@gmail.com>
2021-06-06 20:11:38 +0000

    first commit

---
+++ skibidi/website/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Clients(models.Model):
+    pass
+
+class Booking(models.Model):
+    pass
+
+class Hotels(models.Model):
+    pass
