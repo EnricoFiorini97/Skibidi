e63fac2f4e33f4c9bff7de13048e0e51736391d5
Carmine <carmine.laluna@gmail.com>
2021-06-15 00:09:26 +0200

    adding jsvideo and refactoring website views

--- skibidi/website/views.py
+++ skibidi/website/views.py
@@ -1,37 +1,38 @@
 import env
 import json
 import requests
-from backend.models import Anime
+from backend.models import Anime, Kind, Episode
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from backend.serializers import KindSerializer, AnimeSerializer, EpisodeSerializer
+
+k_list = []
+querysetKind = Kind.objects.all()
+for q in querysetKind:
+     serialized_kind = KindSerializer(q)
+     k_list.append(serialized_kind.data["kind_name"])
 
 def index(request):
-    r = json.loads(requests.get(f'http://{env.MY_IP}:8000/backend/search/serializers/kind/all/').text)
-    kind_list = [r[i]['kind_name'] for i in range(len(r))]
-
     query_set = Anime.objects.order_by('name','season')
     paginator = Paginator(query_set, 18)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'index-2.html', {'kind_list':kind_list, 'page_obj':page_obj})
+    return render(request, 'index-2.html', {'kind_list':k_list, 'page_obj':page_obj})
 
 
 def anime_ep(request, anime, stagione, ep):
-    r = json.loads(requests.get(f'http://{env.MY_IP}:8000/backend/search/serializers/anime/all/').text)
-    for i in range(len(r)):
-        if r[i]["name"] == anime and r[i]['season'] == stagione:
-            identify = r[i]['anime_id']        
-    r = json.loads(requests.get(f'http://{env.MY_IP}:8000/backend/search/serializers/episodes/anime/{identify}').text)
-    if (int(r[0]['name'])) > 1:
-        ep_link = r[ep-(int(r[0]['name']))]['path']
-    else:
-        ep_link = r[ep-1]['path']
-    print(ep_link)
-    return render(request, 'media.html', {'anime': anime,'stagione':stagione,'ep':ep, 'ep_link':ep_link})
+    querysetAnime = Anime.objects.filter(name=anime, season=stagione)
+    for q in querysetAnime:
+        if AnimeSerializer(q).data["name"] == anime and AnimeSerializer(q).data["season"] == stagione: 
+            identify = AnimeSerializer(q).data['anime_id']
+    querysetEpisode = Episode.objects.filter(e_anime=identify, name=str(ep))
+    serialized_Episode = EpisodeSerializer(querysetEpisode[0])
+    return render(request, 'media.html', {'query':querysetAnime[0], 'kind_list':k_list,'anime': anime,'stagione':stagione,'ep':ep, 'ep_link':serialized_Episode.data["path"]})
 
 def anime_ep_list(request, anime, stagione):
-    r = json.loads(requests.get(f'http://{env.MY_IP}:8000/backend/search/serializers/anime/{anime}/seasons').text)
-    return render(request, 'index_episodes.html',{'anime': anime,'stagione':stagione, 'ep_list':range(r[stagione-1]['start_number_episode'], r[stagione-1]['last_episode']+1)})
+    querysetAnime = Anime.objects.filter(name=anime, season=stagione)
+    serialized_Anime = AnimeSerializer(querysetAnime[0])
+    return render(request, 'index_episodes.html',{'anime':anime, 'season':stagione,'kind_list':k_list, 'ep_list':range(serialized_Anime.data["start_number_episode"], serialized_Anime.data["last_episode"]+1)})
 
 def login(request):
     return render(request, 'login.html')
