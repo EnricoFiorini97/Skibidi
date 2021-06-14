092a30887e26387c12745e8f056400319db6198e
Carmine <carmine.laluna@gmail.com>
2021-06-15 00:53:43 +0200

    fixing db destroy

--- skibidi/website/views.py
+++ skibidi/website/views.py
@@ -5,19 +5,19 @@ from backend.models import Anime, Kind, Episode
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from backend.serializers import KindSerializer, AnimeSerializer, EpisodeSerializer
-
+'''
 k_list = []
 querysetKind = Kind.objects.all()
 for q in querysetKind:
      serialized_kind = KindSerializer(q)
      k_list.append(serialized_kind.data["kind_name"])
-
+'''
 def index(request):
     query_set = Anime.objects.order_by('name','season')
     paginator = Paginator(query_set, 18)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'index-2.html', {'kind_list':k_list, 'page_obj':page_obj})
+    return render(request, 'index-2.html', { 'page_obj':page_obj})
 
 
 def anime_ep(request, anime, stagione, ep):
@@ -27,12 +27,12 @@ def anime_ep(request, anime, stagione, ep):
             identify = AnimeSerializer(q).data['anime_id']
     querysetEpisode = Episode.objects.filter(e_anime=identify, name=str(ep))
     serialized_Episode = EpisodeSerializer(querysetEpisode[0])
-    return render(request, 'media.html', {'query':querysetAnime[0], 'kind_list':k_list,'anime': anime,'stagione':stagione,'ep':ep, 'ep_link':serialized_Episode.data["path"]})
+    return render(request, 'media.html', {'query':querysetAnime[0], 'anime': anime,'stagione':stagione,'ep':ep, 'ep_link':serialized_Episode.data["path"]})
 
 def anime_ep_list(request, anime, stagione):
     querysetAnime = Anime.objects.filter(name=anime, season=stagione)
     serialized_Anime = AnimeSerializer(querysetAnime[0])
-    return render(request, 'index_episodes.html',{'anime':anime, 'season':stagione,'kind_list':k_list, 'ep_list':range(serialized_Anime.data["start_number_episode"], serialized_Anime.data["last_episode"]+1)})
+    return render(request, 'index_episodes.html',{'anime':anime, 'season':stagione, 'ep_list':range(serialized_Anime.data["start_number_episode"], serialized_Anime.data["last_episode"]+1)})
 
 def login(request):
     return render(request, 'login.html')
