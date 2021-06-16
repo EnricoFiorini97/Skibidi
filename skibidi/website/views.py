79493a8b307c02fb749dd2c76d82dcfc533f7a5e
Carmine <carmine.laluna@gmail.com>
2021-06-16 12:41:06 +0200

    adding anime

--- skibidi/website/views.py
+++ skibidi/website/views.py
@@ -20,7 +20,7 @@ def index(request):
     paginator = Paginator(query_set, 18)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'index-2.html', {'k_list':kind_inity, 'page_obj':page_obj})
+    return render(request, 'index-2.html', {'kind_list':kind_inity, 'page_obj':page_obj})
 
 
 def anime_ep(request, anime, stagione, ep):
@@ -30,12 +30,12 @@ def anime_ep(request, anime, stagione, ep):
             identify = AnimeSerializer(q).data['anime_id']
     querysetEpisode = Episode.objects.filter(e_anime=identify, name=str(ep))
     serialized_Episode = EpisodeSerializer(querysetEpisode[0])
-    return render(request, 'media.html', {'k_list':kind_inity(), 'query':querysetAnime[0], 'anime': anime,'stagione':stagione,'ep':ep, 'ep_link':serialized_Episode.data["path"]})
+    return render(request, 'media.html', {'kind_list':kind_inity(), 'query':querysetAnime[0], 'anime': anime,'stagione':stagione,'ep':ep, 'ep_link':serialized_Episode.data["path"]})
 
 def anime_ep_list(request, anime, stagione):
     querysetAnime = Anime.objects.filter(name=anime, season=stagione)
     serialized_Anime = AnimeSerializer(querysetAnime[0])
-    return render(request, 'index_episodes.html',{'k_list':kind_inity(), 'anime':anime, 'season':stagione, 'ep_list':range(serialized_Anime.data["start_number_episode"], serialized_Anime.data["last_episode"]+1)})
+    return render(request, 'index_episodes.html',{'kind_list':kind_inity(), 'anime':anime, 'season':stagione, 'ep_list':range(serialized_Anime.data["start_number_episode"], serialized_Anime.data["last_episode"]+1)})
 
 def login(request):
     return render(request, 'login.html')
