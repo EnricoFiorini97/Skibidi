b97ff7591e6ae980836aaf210a7d85d6d955042c
Enrico Fiorini <fiorinienr@gmail.com>
2021-06-20 00:19:51 +0200

    adding env-saple.py

---
+++ skibidi/env-sample.py
@@ -0,0 +1,25 @@
+#Django secret key
+SECRET_KEY = ""
+
+#List of allowed hosts
+ALLOWED_HOSTS = ["127.0.0.1","localhost"]
+
+#Change this to set DB User
+DB_USER = ""
+
+#Cjange this to set DB User password
+DB_PWD = ""
+
+#Change this to set DB name
+DB_NAME = ""
+
+#Change this to set your IP
+MY_IP = ""
+
+#Change this to set credentials for Google's password recovery daemon
+MY_GOOGLE = "your gmail address"
+MY_GOOGLE_PASS = "your password"
+
+#Change this to enable/disable access to Admin panel
+IS_ADMIN_ENABLED = False
+
